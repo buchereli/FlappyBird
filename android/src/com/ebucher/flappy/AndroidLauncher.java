037ec1aa65cec091bd398af1107a9aec4391944d
Eli <buchereli@gmail.com>
2017-06-08 19:10:15 -0400

    Test Commit

--- android/src/com/ebucher/flappy/AndroidLauncher.java
+++ android/src/com/ebucher/flappy/AndroidLauncher.java
@@ -7,10 +7,11 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.ebucher.flappy.FlappyDemo;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new FlappyDemo(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new FlappyDemo(), config);
+        //TEST
+    }
 }
